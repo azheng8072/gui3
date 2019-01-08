867cc5115ebd3c4f970af9270fc383cfa4fe741c
BT_1N3_14@BKTECH.LOCAL <BT_1N3_14@BKTECH.LOCAL>
2019-01-08 11:33:58 -0500

    Initial commit

---
+++ src/sample/gui3.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class gui3 {
+    public Label clicker;
+    public int countedClicks = 0;
+    private boolean counting = false;
+    private boolean done = false;
+
+    public void clickEvent(ActionEvent actionEvent) {
+
+        if(!counting && !done)
+        {
+            long step = System.nanoTime() + 10000000000L;
+            double stepInSec = (double)step / 1_000_000_000.0;
+            new AnimationTimer() {
+                public void handle(long now) {
+                    if(now > step)
+                    {
+                        clicker.setText("You ended with " + countedClicks + " points");
+                        counting = false;
+                        done = true;
+                    }
+                    if (now < step) {
+                        long time2 = now - step;
+                        clicker.setText("Time Left: " + -time2 + " | Points: " + String.valueOf(countedClicks));
+                    }
+                }
+            }.start();
+            counting = true;
+        }
+        if(counting) {
+            countedClicks++;
+            String num = String.valueOf(countedClicks);
+            clicker.setText(num);
+        }
+    }
+}
+
